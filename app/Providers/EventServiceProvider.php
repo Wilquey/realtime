9516435ba01b3e1d11b7a77002a09bc59c088846
wilquey <wilquey@gmail.com>
2022-04-01 09:41:19 -0300

    teste listeners x events

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,22 @@
 
 namespace App\Providers;
 
+use App\Events\{
+    PostCreated
+};
+use App\Listeners\{
+    NotifyUsersNewPostCreated
+};
+use App\Models\{
+    Post
+};
+use App\Observers\{
+    PostObserver
+};
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -18,6 +30,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        PostCreated::class => [
+            NotifyUsersNewPostCreated::class,
+        ]
     ];
 
     /**
@@ -27,6 +42,6 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Post::observe(PostObserver::class);
     }
 }
